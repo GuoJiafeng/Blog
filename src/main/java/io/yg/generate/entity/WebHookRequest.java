4b6d732c864bbd06d25cffacfeeec4aa1645dda5
GuoJiafeng <18611781163@163.com>
2020-07-23 13:33:52 +0800

    init v1

--- src/main/java/io/yg/generate/entity/WebHookRequest.java
+++ src/main/java/io/yg/generate/entity/WebHookRequest.java
@@ -9,4 +9,43 @@ public class WebHookRequest {
     private String ref;
     private String before;
     private String after;
+
+    public WebHookRequest(String ref, String before, String after) {
+        this.ref = ref;
+        this.before = before;
+        this.after = after;
+    }
+
+    public String getRef() {
+        return ref;
+    }
+
+    public void setRef(String ref) {
+        this.ref = ref;
+    }
+
+    public String getBefore() {
+        return before;
+    }
+
+    public void setBefore(String before) {
+        this.before = before;
+    }
+
+    public String getAfter() {
+        return after;
+    }
+
+    public void setAfter(String after) {
+        this.after = after;
+    }
+
+    @Override
+    public String toString() {
+        return "WebHookRequest{" +
+                "ref='" + ref + '\'' +
+                ", before='" + before + '\'' +
+                ", after='" + after + '\'' +
+                '}';
+    }
 }
