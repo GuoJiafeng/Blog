b2446c7aa7fcdea0b517f37ff258e6fd37f01f32
GuoJiafeng <18611781163@163.com>
2020-07-23 14:09:58 +0800

    init v1

--- src/main/java/io/yg/controller/WebHookController.java
+++ src/main/java/io/yg/controller/WebHookController.java
@@ -7,18 +7,19 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author v_guojiafeng
  * @time 2020/7/23 11:44 上午
  * @info
  */
-@Controller
+@RestController
 @RequestMapping("/v1")
 public class WebHookController {
 
     @PostMapping("/getNotice")
-    public @ResponseBody  WebHookRequest getNotice(@RequestBody WebHookRequest webHookRequest) {
+    public WebHookRequest getNotice(@RequestBody WebHookRequest webHookRequest) {
         System.out.println("目前收到了" + webHookRequest.toString());
         return webHookRequest;
     }
