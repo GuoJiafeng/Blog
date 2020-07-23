d7aed71c557cdcfd6d7c14543372bb1e66ef7dfb
GuoJiafeng <18611781163@163.com>
2020-07-23 14:15:20 +0800

    init v1

--- src/main/java/io/yg/controller/WebHookController.java
+++ src/main/java/io/yg/controller/WebHookController.java
@@ -1,13 +1,15 @@
 package io.yg.controller;
 
 import io.yg.generate.entity.WebHookRequest;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpRequest;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author v_guojiafeng
@@ -19,7 +21,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class WebHookController {
 
     @PostMapping("/getNotice")
-    public WebHookRequest getNotice(@RequestBody WebHookRequest webHookRequest) {
+    public WebHookRequest getNotice(@RequestBody WebHookRequest webHookRequest, HttpRequest httpRequest) {
+
+        HttpHeaders headers = httpRequest.getHeaders();
+        for (Map.Entry<String, List<String>> listEntry : headers.entrySet()) {
+            for (String s : listEntry.getValue()) {
+                System.out.println(listEntry.getKey()+":"+s);
+            }
+        }
         System.out.println("目前收到了" + webHookRequest.toString());
         return webHookRequest;
     }
