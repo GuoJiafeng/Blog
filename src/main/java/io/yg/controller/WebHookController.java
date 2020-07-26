9d028b67a7554b7e0cec348cfd941f2a865bb6e5
GuoJiafeng <18611781163@163.com>
2020-07-26 22:33:10 +0800

    update

--- src/main/java/io/yg/controller/WebHookController.java
+++ src/main/java/io/yg/controller/WebHookController.java
@@ -3,6 +3,7 @@ package io.yg.controller;
 
 import io.yg.generate.entity.WebHookRequest;
 import io.yg.service.GenerateResourceService;
+import io.yg.util.ShellUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -34,6 +35,8 @@ public class WebHookController {
         System.out.println("检验为：" + request.getHeader("X-Hub-Signature"));
 
         System.out.println("目前收到了" + webHookRequest.toString());
+
+        System.out.println("当前目录为："+ShellUtil.exceScript("pwd", ShellUtil.LINUX));
 
         generateResourceService.execGenerate();
 
