a1f056c9761898c50e6ac794f210066ee9984d61
GuoJiafeng <18611781163@163.com>
2020-07-27 01:32:10 +0800

    update

--- kill.sh
+++ kill.sh
@@ -6,13 +6,12 @@
 
 pid=`ps -ef | grep run.sh | grep -v "grep" | awk '{print $2}'`
 ishave=`ps -ef | grep run.sh | grep -v "grep" |wc -l`
-echo $pid
 
 if [ $ishave -le 0 ];then
-    echo "没有"
+    echo "没有残留进程"
 
     else
-   "有"
+   echo "有残留进程"
     for id in $pid
 do
     kill -9 $id
