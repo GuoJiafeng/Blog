acb0e5a5b3b7b3e4842c46a1b1b9383dc12534b6
GuoJiafeng <18611781163@163.com>
2020-07-27 01:31:11 +0800

    update

--- kill.sh
+++ kill.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/bin/bash
 
 #################################
 ##   @author guojiafeng        ##
@@ -8,9 +8,11 @@ pid=`ps -ef | grep run.sh | grep -v "grep" | awk '{print $2}'`
 ishave=`ps -ef | grep run.sh | grep -v "grep" |wc -l`
 echo $pid
 
-if [ $ishave -le 0 ];
-    then
-    echo "有"
+if [ $ishave -le 0 ];then
+    echo "没有"
+
+    else
+   "有"
     for id in $pid
 do
     kill -9 $id
