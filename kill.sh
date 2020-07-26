9409e763beb00cea7265ef8388d4d9de98578ee1
GuoJiafeng <18611781163@163.com>
2020-07-27 01:21:39 +0800

    update

--- kill.sh
+++ kill.sh
@@ -7,8 +7,12 @@
 pid=`ps -ef | grep run.sh | grep -v "grep" | awk '{print $2}'`
 echo $pid
 
-for id in $pid
+if [ -n $pid ];
+    then
+    for id in $pid
 do
     kill -9 $id
     echo "killed $id"
 done
+
+fi
