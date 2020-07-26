0d1b337c31b59f352585f261a0627ea528da171f
GuoJiafeng <18611781163@163.com>
2020-07-26 22:31:10 +0800

    update

--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+echo "删除无用文件"
+rm -rf ./*md
+rm -rf ./push_github.sh
 echo "更新 git "
-git pull
+git pull ./
 echo "开始 编译 "
 mvn clean package
 echo "开始运行"
-java -jar target/Blog.jar
+java -jar ./target/Blog.jar
--- src/main/java/io/yg/controller/IndexController.java
+++ src/main/java/io/yg/controller/IndexController.java
@@ -1,11 +1,13 @@
 package io.yg.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/v1")
+
+@Controller
 public class IndexController {
-
-    public void index(){}
+    @RequestMapping("/")
+    public String index() {
+        return "redirect:/index.html";
+    }
 }
