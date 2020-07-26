d8ad2d7f8baea0e3df7db07747117cd7ff2d5934
GuoJiafeng <18611781163@163.com>
2020-07-27 01:14:11 +0800

    update

--- run.sh
+++ run.sh
@@ -10,9 +10,7 @@ echo "#################################"
 echo "查看是否需要关闭残留进程"
 
 
-pid=`ps -ef | grep run.sh | grep -v grep | awk '{print $2}'`
-
-kill -9 $pid
+kill -9 $(ps -ef | grep run.sh | grep -v grep | awk '{print $2}')
 
 echo "删除无用文件"
 rm -rf ./*md
