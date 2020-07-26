f14a3c5e47d0f5b0b1dad6ae099e278191ffd81c
GuoJiafeng <18611781163@163.com>
2020-07-26 22:59:32 +0800

    update

--- src/main/java/io/yg/controller/WebHookController.java
+++ src/main/java/io/yg/controller/WebHookController.java
@@ -37,6 +37,7 @@ public class WebHookController {
         System.out.println("目前收到了" + webHookRequest.toString());
 
         System.out.println("当前目录为："+ShellUtil.exceScript("pwd", ShellUtil.LINUX));
+        System.out.println("打印的消息为："+ShellUtil.exceScript("echo ~/html/", ShellUtil.LINUX));
 
         generateResourceService.execGenerate();
 
