24b842417c52ae27cdb1954f962932c44f6f628e
v_guojiafeng <v_guojiafeng@baidu.com>
2020-08-17 21:21:34 +0800

    update

--- src/main/java/io/yg/config/CommonConfig.java
+++ src/main/java/io/yg/config/CommonConfig.java
@@ -1,7 +1,9 @@
 package io.yg.config;
 
 import lombok.Data;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 /**
@@ -11,9 +13,13 @@ import org.springframework.stereotype.Component;
  */
 @Data
 @Component
-@ConfigurationProperties(prefix = "io.gjf")
+@Configuration
+@PropertySource("{classpath:application.yml}")
 public class CommonConfig {
+    @Value("${io.gjf.gitpath}")
     private String gitpath;
+    @Value("${io.gjf.nginxpath}")
     private String nginxpath;
+    @Value("${io.gjf.giturl}")
     private String giturl;
 }
