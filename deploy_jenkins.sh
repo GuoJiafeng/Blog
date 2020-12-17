73051065e0086abcbd77a4d59ab687ec02eb20c0
GuoJiafeng <iamgjf@qq.com>
2020-12-18 01:34:02 +0800

    update

--- deploy_jenkins.sh
+++ deploy_jenkins.sh
@@ -3,6 +3,7 @@ export JAVA_HOME=/root/jdk1.8.0_271/
 export  PATH=/root/jdk1.8.0_271/bin:/root/apache-maven-3.6.3/bin:$PATH
 
 mvn clean package
+wget -O opentracing-specialagent-1.7.4.jar "https://repo1.maven.org/maven2/io/opentracing/contrib/specialagent/opentracing-specialagent/1.7.1/opentracing-specialagent-1.7.1.jar"
 
 # 创建新镜像
 docker build -t thisisgjf/blog . && \
