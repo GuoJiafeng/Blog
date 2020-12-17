b4d47c120ef3b41d10980442f87e618546b2ae4d
GuoJiafeng <iamgjf@qq.com>
2020-12-18 01:45:20 +0800

    update

--- deploy_jenkins.sh
+++ deploy_jenkins.sh
@@ -1,8 +1,10 @@
 ## mvn编译
 export JAVA_HOME=/root/jdk1.8.0_271/
-export  PATH=/root/jdk1.8.0_271/bin:/root/apache-maven-3.6.3/bin:$PATH
+
+export PATH=/var/jenkins_home/jdk1.8.0_271/bin:/var/jenkins_home/apache-maven-3.6.3/bin:$PATH
 
 mvn clean package
+
 wget -O opentracing-specialagent-1.7.4.jar "https://repo1.maven.org/maven2/io/opentracing/contrib/specialagent/opentracing-specialagent/1.7.1/opentracing-specialagent-1.7.1.jar"
 
 # 创建新镜像
